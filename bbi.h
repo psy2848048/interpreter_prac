0bd7859937c9810628922ddc03a811898ffbabde
Bryan Rhee <psy2848048@gmail.com>
2019-05-16 23:50:23 +0900

    Start from header

---
+++ bbi.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
